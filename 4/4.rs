b8f56e5f8462c8102479f63722be092dc05e98f1
Erin Hensel <hens0093@gmail.com>
2019-12-10 18:39:17 -0700

    Update day 4 style

--- 4/4.rs
+++ 4/4.rs
@@ -1,30 +1,22 @@
 
 fn test_val(input: i32) -> bool {
-    let get_digit = |input: i32, place: u32| -> i32 {
+    let get_digit = |place: u32| -> i32 {
         (input / 10i32.pow(place)) % 10
     };
     let mut highest = 0i32;
     let mut counts = [0; 10];
     for n in (0..6).rev() {
-        let digit = get_digit(input, n);
+        let digit = get_digit(n);
         if digit < highest {
             return false;
         }
         highest = digit;
         counts[digit as usize] += 1;
     }
-    if counts.iter().any(|&n| n == 2) {
-        return true;
-    }
-    false
+    counts.iter().any(|&n| n == 2)
 }
 
 fn main() {
-    let mut total = 0u32;
-    for n in 240920..=789857 {
-        if test_val(n) {
-            total += 1;
-        }
-    }
+    let total = (240920..=789857).filter(|&n| test_val(n)).count();
     println!("Found {} in range that meet criteria.", total);
 }
